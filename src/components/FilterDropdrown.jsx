1347505b89b323540e81e55c91ceb3e93871a93c
JayeshX <jayeshkirtane218@gmail.com>
2025-10-18 11:59:43 +0530

    assets

--- src/components/FilterDropdrown.jsx
+++ src/components/FilterDropdrown.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react';
+import { ReactComponent as DownIcon } from "../assets/test_Filter.svg";
 
 export function FilterDropdown() {
   const [isOpen, setIsOpen] = useState(false);
   
   return (
     <div className="filter-container">
-      <button onClick={() => setIsOpen(!isOpen)}>
-        🔽 Add Filters
-      </button>
+      <button onClick={() => setIsOpen(!isOpen)} className="flex items-center gap-1">
+        <DownIcon
+            className={`w-4 h-4 transition-transform duration-200 ${
+            isOpen ? "rotate-180" : ""
+            }`}
+        />
+        Add Filters
+        </button>
       {isOpen && (
         <div className="filter-menu">
           <div className="filter-item">Filter 1</div>
